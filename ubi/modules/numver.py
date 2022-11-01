39785e014ed025251a2f2067847fbbc728aa1969
Robi <robelmezemir@gmail.com>
2022-11-01 15:11:02 +0300

    fixes

--- ubi/modules/numver.py
+++ ubi/modules/numver.py
@@ -6,7 +6,7 @@ from telethon import events
 from telethon.tl.types import InputPeerUser
 from ubi import u, s
 
-
+del_timeout = 6
 @u.on(events.NewMessage(pattern=r"\.nv (.*)|\.nv"))
 async def _(event):
     if event.fwd_from:
@@ -14,12 +14,12 @@ async def _(event):
     out = event.message.out
     para = event.pattern_match.group(1)
     para_split = para.split(" ")
-    # check for digit-ness 
-    if not para or len(para_split) < 2  or \
-        not para_split[1].isdigit()  or not para_split[0].isdigit():
+    # check for digit-ness
+    if not para or len(para_split) < 2 or \
+            not para_split[1].isdigit() or not para_split[0].isdigit():
         if out:
             await event.edit("Usage : `.nv 251910101010 <code>`")
-            await asyncio.sleep(4)
+            await asyncio.sleep(del_timeout)
             await event.delete()
         else:
             await event.respond("Usage : `.nv 251910101010 <code>`")
@@ -29,12 +29,23 @@ async def _(event):
         try:
             ent = await u.get_input_entity(para_split[0])
             if isinstance(ent, InputPeerUser):
-                await event.edit("Sending code ")
-                await u.send_message(ent , f"Verification code : `{para_split[1]}`")
-                await event.edit("Sent code!")
+                if out:
+                    await event.edit("Sending code ")
+                    await u.send_message(ent, f"Verification code : `{para_split[1]}`")
+                    await event.edit("Sent code!")
+                    await asyncio.sleep(del_timeout)
+                    await event.delete()
+                else :
+                    resp = await event.respond("Sending code ...")
+                    await u.send_message(ent, f"Verification code : `{para_split[1]}`")
+                    await resp.edit("Sent code!")
+                    await asyncio.sleep(del_timeout)
+                    await resp.delete()
             else:
-                await event.edit("Not a valid phone number or user doesnt have telegram installed.")
+                resp = await event.respond("Not a valid phone number or user doesnt have telegram installed.")
+                await asyncio.sleep(del_timeout)
+                await resp.delete()
         except ValueError:
-            await event.edit("Not a valid phone number or user doesnt have telegram installed.")
-
-
+                resp = await event.respond("Not a valid phone number or user doesnt have telegram installed.")
+                await asyncio.sleep(del_timeout)
+                await resp.delete()
