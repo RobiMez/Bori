d61c24b25c8a5febe6b32871f5d58fb9fe369c42
Robimez <robelmezemir@gmail.com>
2022-11-09 12:30:33 +0300

    i reallly gotta go pee

--- ubi/modules/die.py
+++ ubi/modules/die.py
@@ -1,10 +1,10 @@
 from telethon import events
 from ubi import u 
 from datetime import datetime
-import asyncio
+import asyncio , re 
 
 
-@u.on(events.NewMessage(pattern= re.compile(r"\.ping") , outgoing=True))  # pylint: disable=E0602
+@u.on(events.NewMessage(pattern= re.compile(r"\.die") , outgoing=True))  # pylint: disable=E0602
 async def _(event):
     if event.fwd_from:
         return
