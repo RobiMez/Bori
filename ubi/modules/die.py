d6a707579998d982843457862d7319916d1af0db
Robimez <robelmezemir@gmail.com>
2023-01-14 17:17:12 +0300

    killcode for die

--- ubi/modules/die.py
+++ ubi/modules/die.py
@@ -1,11 +1,17 @@
 from telethon import events
-from ubi import u 
-import  re 
+from ubi import u
+import re
+from ubi.modules.strings import KILL_CODE
 
 
-@u.on(events.NewMessage(pattern= re.compile(r"\.die")))  # pylint: disable=E0602
+@u.on(events.NewMessage(pattern=re.compile(r"\.die (.*)")))
 async def _(event):
     if event.fwd_from:
         return
-    await event.reply("Commiting Soduku ... 💀")
-    await u.disconnect()
+    killcode = event.pattern_match.group(1)
+    print(killcode)
+    if killcode == KILL_CODE:
+        await event.reply("Valid Killcode Entered  ... \nDisconnecting ! 💀")
+        await u.disconnect()
+    else:
+        await event.reply("Kill Code Invalid  ... \n")
---
+++ ubi/modules/strings.py
@@ -0,0 +1,2 @@
+# bruh i know i put it in plain sight - shhhh
+KILL_CODE = "kermitsewerslide"
